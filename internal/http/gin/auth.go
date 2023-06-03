dd3e55aa1caf72a1c43efe565eff8f4130d0f9ed
Mohammed Ali <mohaali482@gmail.com>
2023-06-03 13:35:27 +0300

    Added create handler for auth users

--- internal/http/gin/auth.go
+++ internal/http/gin/auth.go
@@ -1,13 +1,43 @@
 package gin
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 	"github.com/mohaali/goAuth/auth"
+	"github.com/mohaali/goAuth/errors"
 )
 
 func Handlers(s auth.UserService) *gin.Engine {
 	r := gin.Default()
+	r.Handle("POST", "/users", Create(s))
 
 	return r
+
+}
+
+func Create(s auth.UserService) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var user auth.User
+		err := c.ShouldBind(&user)
+		if err != nil {
+			c.AbortWithStatus(http.StatusUnprocessableEntity)
+			return
+		}
+
+		err = user.Validate()
+		if err != nil {
+			errors.ReturnErrorResponse(err, c)
+			return
+		}
+
+		user, err = s.Create(user)
+		if err != nil {
+			c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		c.JSON(http.StatusCreated, user)
+	}
 
 }
