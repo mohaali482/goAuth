3d4c834f710e8b3f80757a3bf9bcf5444f8cf90c
Mohammed Ali <mohaali482@gmail.com>
2023-06-03 16:13:28 +0300

    Added user login

--- auth/auth.go
+++ auth/auth.go
@@ -109,3 +109,20 @@ type Repository interface {
 	Update(id int, user User) (User, error)
 	Delete(id int) error
 }
+
+type UserLogin struct {
+	Username string `json:"username" validate:"required"`
+	Password string `json:"password" validate:"required"`
+}
+
+func (u *UserLogin) Validate() error {
+	validate := validator.New()
+	validate.RegisterTagNameFunc(func(fld reflect.StructField) string {
+		name := strings.SplitN(fld.Tag.Get("json"), ",", 2)[0]
+		if name == "-" {
+			return ""
+		}
+		return name
+	})
+	return validate.Struct(u)
+}
