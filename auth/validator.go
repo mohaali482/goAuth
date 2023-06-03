edc941c60db19d04734673579a970591b43c8f59
Mohammed Ali <mohaali482@gmail.com>
2023-06-03 22:46:31 +0300

    Added a function to register tags to validator

--- auth/validator.go
+++ auth/validator.go
@@ -9,12 +9,16 @@ import (
 
 func Validate(i interface{}) error {
 	validate := validator.New()
-	validate.RegisterTagNameFunc(func(fld reflect.StructField) string {
+	RegisterTagNameFunc(validate)
+	return validate.Struct(i)
+}
+
+func RegisterTagNameFunc(v *validator.Validate) {
+	v.RegisterTagNameFunc(func(fld reflect.StructField) string {
 		name := strings.SplitN(fld.Tag.Get("json"), ",", 2)[0]
 		if name == "-" {
 			return ""
 		}
 		return name
 	})
-	return validate.Struct(i)
 }
