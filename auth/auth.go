5a7e76d134b95440fd35183165da91f24bcad640
Mohammed Ali <mohaali482@gmail.com>
2023-06-03 16:06:46 +0300

    Added phone validator

--- auth/auth.go
+++ auth/auth.go
@@ -75,6 +75,18 @@ func (u *User) Validate() error {
 	return validate.Struct(u)
 }
 
+func (u *User) ValidatePhone() error {
+	validate := validator.New()
+	validate.RegisterTagNameFunc(func(fld reflect.StructField) string {
+		name := strings.SplitN(fld.Tag.Get("json"), ",", 2)[0]
+		if name == "-" {
+			return ""
+		}
+		return name
+	})
+	return validate.StructPartial(u, "Phone")
+}
+
 type UseCase interface {
 	Create(user User) (User, error)
 	GetAll() (Users, error)
